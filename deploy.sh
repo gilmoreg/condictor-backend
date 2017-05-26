080adc57205fc266d40c49ebd4ad6c56f6e0324d
Grayson Gilmore <gilmoreg@live.com>
2017-05-26 18:31:42 -0500

    Switch to heroku

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,8 @@ then
 fi
 
 # Install the toolbelt, and the required plugin.
-# docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD
-docker build -f Dockerfile -t $DOCKER_IMAGE_NAME .
-# if [ ! -z "$TRAVIS_TAG" ]; then 
-#  docker tag $DOCKER_IMAGE_NAME:latest $DOCKER_IMAGE_NAME:$TRAVIS_TAG; 
-# fi 
-# if [ "$TRAVIS_BRANCH" == "master" ]; then 
-#  docker push $DOCKER_IMAGE_NAME; 
-# fi
-
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install heroku-container-registry
+docker login -u=$DOCKER_USERNAME -p=$HEROKU_API_KEY registry.heroku.com
+heroku container:push --app condictor-backend
 
