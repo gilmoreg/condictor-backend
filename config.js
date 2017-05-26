417a4d5c42793609dc8774ca38ef5e3b900aa63c
Grayson Gilmore <gilmoreg@live.com>
2017-05-26 15:06:46 -0500

    Change port

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_USER ? `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PW}@${process.env.DATABASE_URL}`
     : 'mongodb://localhost/condictor',
   TEST_DATABASE_URL: 'mongodb://localhost/test-condictor',
-  PORT: process.env.PORT || 3001,
-  TEST_PORT: 3002,
+  PORT: process.env.PORT || 3000,
+  TEST_PORT: 3001,
 };
